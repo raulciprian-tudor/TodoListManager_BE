746fff5f4fb77872f696dc6169d49c1bacf88604
Tudor Raul Ciprian <78701525+raulciprian-tudor@users.noreply.github.com>
2024-10-14 09:25:48 +0200

    update backend config

---
+++ Controllers/TestGet.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo_List_Manager___BE.Controllers
+{
+    [ApiController]
+    [Route("Test")]
+    public class TestGet : ControllerBase
+    {
+        [HttpGet]
+        [Route("Get")]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
